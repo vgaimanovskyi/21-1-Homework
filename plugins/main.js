c68f552645d7f8ac5dd57990c0da8c766032ca30
mak7iliano <mak7iliano@users.noreply.github.com>
2020-04-11 11:05:58 +0300

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -1,5 +1,9 @@
 $(function () {
     //ie detection
+    if (!!window.MSInputMethodContext && !!document.documentMode) {
+        $("html").addClass("ie11");
+    }
+    
     if ($.browser.msie && $.browser.version == 10) {
         $("html").addClass("ie10");
     }
